1a3ff38000e57b2e770fa071a04258ee6fa8c667
1Gian <giansemi@gmail.com>
2015-10-27 20:25:11 +0300

    kicc app init

---
+++ app/src/main/java/com/example/gian/kicc/login.java
@@ -0,0 +1,17 @@
+package com.example.gian.kicc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gian on 10/27/15.
+ */
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+}
